c3ceaef4c2c06d1a6a42e368a2a88e1c89293d12
Vitalii <vpempus@gmail.com>
2020-06-11 14:38:59 +0300

    change start

--- CarLotMVC/App_Start/RouteConfig.cs
+++ CarLotMVC/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace CarLotMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Inventory", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
